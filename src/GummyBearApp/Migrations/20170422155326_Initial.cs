0047e1c9c297fdde036ab9d955f2f3630564126f
Sam Kirsch <samdkirsch@gmail.com>
2017-04-22 09:00:41 -0700

    initialize database

---
+++ src/GummyBearApp/Migrations/20170422155326_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GummyBearApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GummyBear",
+                columns: table => new
+                {
+                    GummyBearId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cost = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: true),
+                    ImageAlt = table.Column<string>(nullable: true),
+                    ImageSrc = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GummyBear", x => x.GummyBearId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GummyBear");
+        }
+    }
+}
---
+++ src/GummyBearApp/Migrations/GummyBearDbContextModelSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GummyBearApp.Models;
+
+namespace GummyBearApp.Migrations
+{
+    [DbContext(typeof(GummyBearDbContext))]
+    partial class GummyBearDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("GummyBearApp.Models.GummyBear", b =>
+                {
+                    b.Property<int>("GummyBearId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cost");
+
+                    b.Property<string>("Country");
+
+                    b.Property<string>("ImageAlt");
+
+                    b.Property<string>("ImageSrc");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("GummyBearId");
+
+                    b.ToTable("GummyBear");
+                });
+        }
+    }
+}
