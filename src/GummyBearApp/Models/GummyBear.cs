5020bd45a60ca19b0a9f97d48085404e324f2b2d
Sam Kirsch <samdkirsch@gmail.com>
2017-04-22 08:32:59 -0700

    create gummybear model

---
+++ src/GummyBearApp/Models/GummyBear.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummyBearApp.Models
+{
+    public class GummyBear
+    {
+    }
+}
---
+++ src/GummyBearApp/Models/GummyBearDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummyBearApp.Models
+{
+    [Table("GummyBear")]
+    public class GummyBear
+    {
+        [Key]
+        public int GummyBearId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Cost { get; set; }
+        public string ImageSrc { get; set; }
+        public string ImageAlt { get; set; }
+
+    }
+}
